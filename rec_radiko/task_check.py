99e6e9b6b4e941f8f1b8b934cb4200b073b9f9f8
homirun <hatunemiku.moco@gmail.com>
2018-11-28 15:38:59 +0900

    リファクタリング

--- rec_radiko/task_check.py
+++ rec_radiko/task_check.py
@@ -13,7 +13,7 @@ def connect_sqlite():
     date = datetime.now().strftime('%Y-%m-%d')
     conn = sqlite3.connect('../db.sqlite3')
     cursor = conn.cursor()
-    for item in cursor.execute('''SELECT * FROM rec_radiko_reserve WHERE start_date '''):
+    for item in cursor.execute('''SELECT * FROM rec_radiko_reserve '''):
         if item[2] == date and item[3][0:2] == time[0:2] and int(time[2:4]) - int(item[3][3:5]) < 1:
             print(os.system('bash ../rec_radiko.sh' + ' ' + item[1] + ' ' + item[4] + ' ' + '../out/'))
 
